61de7d9397974c3b25c2afbe8b414e56dc7bd68a
Evgeniy Shishkin <eshishki@gmail.com>
2025-02-23 18:41:34 +0400

    tuple_desc removal

--- be/src/exec/hdfs_scanner.cpp
+++ be/src/exec/hdfs_scanner.cpp
@@ -133,7 +133,7 @@ Status HdfsScanner::_build_scanner_context() {
         ctx.partition_columns.emplace_back(std::move(column));
     }
 
-    ctx.tuple_desc = _scanner_params.tuple_desc;
+    ctx.slot_descs = _scanner_params.tuple_desc->slots();
     ctx.scan_range = _scanner_params.scan_range;
     ctx.runtime_filter_collector = _scanner_params.runtime_filter_collector;
     ctx.min_max_conjunct_ctxs = _scanner_params.min_max_conjunct_ctxs;
