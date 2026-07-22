fe9809c3a10cc7c0dbcf8e037579c5963cad7817
Tim Wendisch <t.wendisch@celonis.com>
2026-07-22 07:00:51 +0200

    [Enhancement] Add statistics for LTRIM binary (#76605)
    Signed-off-by: Tim Wendisch <t.wendisch@celonis.com>
    (cherry picked from commit eb830babf2ec74b7ba499dfc57228a0fc520b9c8)

--- fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ExpressionStatisticCalculator.java
+++ fe/fe-core/src/main/java/com/starrocks/sql/optimizer/statistics/ExpressionStatisticCalculator.java
@@ -1016,6 +1016,13 @@ public class ExpressionStatisticCalculator {
                     break;
                 case FunctionSet.DATE_TRUNC:
                     return calculateDateTruncStats(callOperator, right);
+                case FunctionSet.LTRIM:
+                case FunctionSet.LTRIM_STRING:
+                    minValue = Double.NEGATIVE_INFINITY;
+                    maxValue = Double.POSITIVE_INFINITY;
+                    averageRowSize = left.getAverageRowSize();
+                    distinctValues = Math.min(rowCount, left.getDistinctValuesCount());
+                    break;
                 default:
                     return ColumnStatistic.unknown();
             }
